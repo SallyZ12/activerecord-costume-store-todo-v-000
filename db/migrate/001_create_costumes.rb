98825ef15a6fe34388da57d7dee0553c9f8a514c
Sally Zasloff <szasloff@gmail.com>
2018-01-13 16:15:22 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class Costumes < ActiveRecord::Migration[4.2]
   def change
-    create_table costumes do |t|
+    create_table :costumes do |t|
       t.string :name
       t.integer :price
       t.string :size
